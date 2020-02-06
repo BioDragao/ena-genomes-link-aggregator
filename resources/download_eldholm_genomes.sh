e957e2e82bbaeb716c1f81bcba97ec9ef81dcec5
Abhinav Sharma <abhi18av@gmail.com>
2020-02-06 11:59:30 -0300

    create the download script

---
+++ resources/download_eldholm_genomes.sh
@@ -0,0 +1,162 @@
+#BRZ-260
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/002/ERR2653212/ERR2653212_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/002/ERR2653212/ERR2653212_2.fastq.gz"
+#BRZ-116
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/005/ERR2653085/ERR2653085_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/005/ERR2653085/ERR2653085_2.fastq.gz"
+#BRZ-265
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/008/ERR2653238/ERR2653238_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/008/ERR2653238/ERR2653238_2.fastq.gz"
+#BRZ-266
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/009/ERR2653239/ERR2653239_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/009/ERR2653239/ERR2653239_2.fastq.gz"
+#BRZ-267
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/000/ERR2653240/ERR2653240_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/000/ERR2653240/ERR2653240_2.fastq.gz"
+#BRZ-268
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/001/ERR2653241/ERR2653241_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/001/ERR2653241/ERR2653241_2.fastq.gz"
+#BRZ-269
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/002/ERR2653202/ERR2653202_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/002/ERR2653202/ERR2653202_2.fastq.gz"
+#BRZ-117
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/004/ERR2653084/ERR2653084_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/004/ERR2653084/ERR2653084_2.fastq.gz"
+#BRZ-270
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/003/ERR2653203/ERR2653203_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/003/ERR2653203/ERR2653203_2.fastq.gz"
+#BRZ-271
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/004/ERR2653204/ERR2653204_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/004/ERR2653204/ERR2653204_2.fastq.gz"
+#BRZ-272
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/005/ERR2653205/ERR2653205_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/005/ERR2653205/ERR2653205_2.fastq.gz"
+#BRZ-273
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/006/ERR2653206/ERR2653206_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/006/ERR2653206/ERR2653206_2.fastq.gz"
+#BRZ-274
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/007/ERR2653207/ERR2653207_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/007/ERR2653207/ERR2653207_2.fastq.gz"
+#BRZ-275
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/008/ERR2653208/ERR2653208_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/008/ERR2653208/ERR2653208_2.fastq.gz"
+#BRZ-276
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/009/ERR2653209/ERR2653209_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/009/ERR2653209/ERR2653209_2.fastq.gz"
+#BRZ-277
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/000/ERR2653210/ERR2653210_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/000/ERR2653210/ERR2653210_2.fastq.gz"
+#BRZ-113
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/008/ERR2653088/ERR2653088_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/008/ERR2653088/ERR2653088_2.fastq.gz"
+#BRZ-114
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/007/ERR2653087/ERR2653087_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/007/ERR2653087/ERR2653087_2.fastq.gz"
+#BRZ-115
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/006/ERR2653086/ERR2653086_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/006/ERR2653086/ERR2653086_2.fastq.gz"
+#BRZ-294
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/008/ERR2653228/ERR2653228_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/008/ERR2653228/ERR2653228_2.fastq.gz"
+#BRZ-119
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/003/ERR2653083/ERR2653083_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/003/ERR2653083/ERR2653083_2.fastq.gz"
+#BRZ-283
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/007/ERR2653217/ERR2653217_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/007/ERR2653217/ERR2653217_2.fastq.gz"
+#BRZ-120
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/002/ERR2653082/ERR2653082_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/002/ERR2653082/ERR2653082_2.fastq.gz"
+#BRZ-121
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/001/ERR2653081/ERR2653081_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/001/ERR2653081/ERR2653081_2.fastq.gz"
+#BRZ-122
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/000/ERR2653080/ERR2653080_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/000/ERR2653080/ERR2653080_2.fastq.gz"
+#BRZ-123
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/009/ERR2653079/ERR2653079_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/009/ERR2653079/ERR2653079_2.fastq.gz"
+#BRZ-124
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/008/ERR2653078/ERR2653078_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/008/ERR2653078/ERR2653078_2.fastq.gz"
+#BRZ-126
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/007/ERR2653077/ERR2653077_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/007/ERR2653077/ERR2653077_2.fastq.gz"
+#BRZ-127
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/006/ERR2653076/ERR2653076_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/006/ERR2653076/ERR2653076_2.fastq.gz"
+#BRZ-291
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/005/ERR2653225/ERR2653225_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/005/ERR2653225/ERR2653225_2.fastq.gz"
+#BRZ-292
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/006/ERR2653226/ERR2653226_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/006/ERR2653226/ERR2653226_2.fastq.gz"
+#BRZ-293
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/007/ERR2653227/ERR2653227_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/007/ERR2653227/ERR2653227_2.fastq.gz"
+#BRZ-280
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/004/ERR2653214/ERR2653214_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/004/ERR2653214/ERR2653214_2.fastq.gz"
+#BRZ-295
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/009/ERR2653229/ERR2653229_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/009/ERR2653229/ERR2653229_2.fastq.gz"
+#BRZ-297
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/000/ERR2653230/ERR2653230_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/000/ERR2653230/ERR2653230_2.fastq.gz"
+#BRZ-259
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/001/ERR2653201/ERR2653201_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/001/ERR2653201/ERR2653201_2.fastq.gz"
+#BRZ-285
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/008/ERR2653218/ERR2653218_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/008/ERR2653218/ERR2653218_2.fastq.gz"
+#BRZ-261
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/003/ERR2653223/ERR2653223_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/003/ERR2653223/ERR2653223_2.fastq.gz"
+#BRZ-263
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/003/ERR2653233/ERR2653233_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/003/ERR2653233/ERR2653233_2.fastq.gz"
+#BRZ-264
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/007/ERR2653237/ERR2653237_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/007/ERR2653237/ERR2653237_2.fastq.gz"
+#BRZ-303
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/006/ERR2653236/ERR2653236_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/006/ERR2653236/ERR2653236_2.fastq.gz"
+#BRZ-278
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/001/ERR2653211/ERR2653211_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/001/ERR2653211/ERR2653211_2.fastq.gz"
+#BRZ-279
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/003/ERR2653213/ERR2653213_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/003/ERR2653213/ERR2653213_2.fastq.gz"
+#BRZ-288
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/001/ERR2653221/ERR2653221_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/001/ERR2653221/ERR2653221_2.fastq.gz"
+#BRZ-298
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/001/ERR2653231/ERR2653231_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/001/ERR2653231/ERR2653231_2.fastq.gz"
+#BRZ-281
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/005/ERR2653215/ERR2653215_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/005/ERR2653215/ERR2653215_2.fastq.gz"
+#BRZ-282
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/006/ERR2653216/ERR2653216_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/006/ERR2653216/ERR2653216_2.fastq.gz"
+#BRZ-289
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/002/ERR2653222/ERR2653222_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/002/ERR2653222/ERR2653222_2.fastq.gz"
+#BRZ-286
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/009/ERR2653219/ERR2653219_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/009/ERR2653219/ERR2653219_2.fastq.gz"
+#BRZ-287
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/000/ERR2653220/ERR2653220_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/000/ERR2653220/ERR2653220_2.fastq.gz"
+#BRZ-300
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/002/ERR2653232/ERR2653232_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/002/ERR2653232/ERR2653232_2.fastq.gz"
+#BRZ-302
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/005/ERR2653235/ERR2653235_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/005/ERR2653235/ERR2653235_2.fastq.gz"
+#BRZ-290
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/004/ERR2653224/ERR2653224_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/004/ERR2653224/ERR2653224_2.fastq.gz"
+#BRZ-301
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/004/ERR2653234/ERR2653234_1.fastq.gz"
+aria2c "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR265/004/ERR2653234/ERR2653234_2.fastq.gz"
